a3950e534c1fcbb1919373e4a7e84dfc3e6a561e
Nguyen Chau Thach <thachnc2002@gmail.com>
2023-10-23 21:43:28 +0700

    udate onboarding screen

--- src/screens/onboarding/OnBoarding.jsx
+++ src/screens/onboarding/OnBoarding.jsx
@@ -5,13 +5,33 @@ import { images } from "../../constants";
 import styles from "./onboarding.style";
 import { MyText } from "../../components";
 
-const OnBoarding = ({ params }) => {
-	const [img, setImg] = useState(images.boarding1);
-	const [text, setText] = useState({
+const introText = [
+	{
 		title: "Cất giữ tiền một cách tiện lợi.",
 		description: "Hoàn tiền lên đến 5% cho mỗi giao dịch và chi tiêu dễ dàng",
-	});
-	const handleChange = () => {};
+	},
+	{
+		title: "Cất giữ tiền một cách tiện lợi 2.",
+		description: "Hoàn tiền lên đến 5% cho mỗi giao dịch và chi tiêu dễ dàng",
+	},
+	{
+		title: "Cất giữ tiền một cách tiện lợi 3.",
+		description: "Hoàn tiền lên đến 5% cho mỗi giao dịch và chi tiêu dễ dàng",
+	},
+];
+
+const OnBoarding = ({ navigation }) => {
+	const [count, setCount] = useState(1);
+	const [img, setImg] = useState(images.boarding1);
+	const [text, setText] = useState(introText[0]);
+	const handleChange = (e) => {
+		if (count < 3) {
+			setText(introText[count]);
+			setCount((prev) => prev + 1);
+		} else {
+			navigation.navigate("Welcome");
+		}
+	};
 	return (
 		<View style={styles.container}>
 			<Image style={styles.logo} source={images.logo} />
@@ -26,7 +46,7 @@ const OnBoarding = ({ params }) => {
 							{text.description}
 						</MyText>
 						<View>
-							<Button title="next" onPress={handleChange} />
+							<Button title="next" onPress={(e) => handleChange(e)} />
 						</View>
 					</View>
 				</ImageBackground>
